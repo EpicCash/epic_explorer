db452c2f78afd8a151a062708d5f83e8e118fc0c
who-biz <37732338+who-biz@users.noreply.github.com>
2024-03-21 16:55:13 -0400

    Fix location component loading modules server side
    - Explorer could not run in this state

--- src/app/view/location/location.component.ts
+++ src/app/view/location/location.component.ts
@@ -1,42 +1,55 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import * as L from 'leaflet';
-import 'leaflet.markercluster'; // Import the marker cluster library
+//import * as L from 'leaflet';
+//import 'leaflet.markercluster'; // Import the marker cluster library
+declare var require: any;
 
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.css']
 })
+
+
 export class LocationComponent implements AfterViewInit {
+  isBrowser: boolean;
+  private L = null;
   private map;
   private locations = [];
-  private markerClusterGroup: L.MarkerClusterGroup; // Create a marker cluster group
+  private markerClusterGroup = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object, private http: HttpClient) {
+      this.isBrowser = isPlatformBrowser(this.platformId);
+    }
 
   ngAfterViewInit(): void {
-    this.initMap();
-    this.fetchLocationData();
+    if (this.isBrowser) {
+      this.L = require('leaflet');
+      this.markerClusterGroup = this.L.MarkerClusterGroup;
+      this.initMap();
+      this.fetchLocationData();
+    }
   }
 
   private initMap(): void {
     const isMobile = window.innerWidth < 768;
-    this.map = L.map('map', {
+    this.map = this.L.map('map', {
       center: [isMobile ? 50 : 30, isMobile ? 20 : -20],
       zoom: 3,
-      maxBounds:  L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180)) , // Disable horizontal scrolling
+      maxBounds:  this.L.latLngBounds(this.L.latLng(-90, -180), this.L.latLng(90, 180)) , // Disable horizontal scrolling
       bounceAtZoomLimits:true,
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    const tiles = this.L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
       minZoom: 2,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
     tiles.addTo(this.map);
-    this.markerClusterGroup = L.markerClusterGroup(); // Initialize the marker cluster group
+    this.markerClusterGroup = this.L.markerClusterGroup(); // Initialize the marker cluster group
     this.markerClusterGroup.addTo(this.map); // Add it to the map
   }
 
@@ -51,19 +64,19 @@ export class LocationComponent implements AfterViewInit {
       .filter(e => e !== null);
 
     for (const location of arrayOfLoc) {
-      const customIcon = L.icon({
+      const customIcon = this.L.icon({
         iconUrl: "/assets/img/icons8-location-80.png",
         iconSize: [32, 32], // Adjust the icon size as needed
         iconAnchor: [16, 32], // Adjust the icon anchor as needed
       });
-      const marker = L.marker([location.lat, location.lng],{ icon: customIcon });
+      const marker = this.L.marker([location.lat, location.lng],{ icon: customIcon });
       marker.bindPopup(location.name);
       this.markerClusterGroup.addLayer(marker); // Add marker to the cluster group
     }
   }
 
   private fetchLocationData(): void {
-    this.http.get('https://explorer.epic.tech/epic_explorer/v1/blockchain_block/v1/peers/all').subscribe((data: any) => {
+    this.http.get('https://explorer.epiccash.com/epic_explorer/v1/blockchain_block/v1/peers/all').subscribe((data: any) => {
       this.locations = data.response;
       this.addMarkers();
     });
